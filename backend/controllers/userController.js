e3b316d35988f84a50ce7562a6ecf55864ef6887
Aarti Lad <120771285+teabytes@users.noreply.github.com>
2023-10-22 18:56:59 +0530

    Add files via upload

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,25 @@
 const User = require('../models/user');
 
 exports.createUser = async (req, res) => {
-  try {
     const user = new User(req.body);
-    await user.save();
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+    const isPresent = await User.findOne(req.body);
+    // console.log(isPresent);
+    if (!isPresent) {
+      // user does not exist
+      // create new user and save to db
+      try {
+        await user.save();
+        res.status(201).json(user);
+      }
+      catch (error) {
+        res.status(400).json({ message: error.message });
+      }
+    }
+    else {
+      // render new page
+      console.log("new page");
+    }
+    
 };
 
 exports.getUser = async (req, res) => {
